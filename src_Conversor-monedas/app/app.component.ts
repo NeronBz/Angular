5ba4a32da13ef59149c713058b33dbf4441da9b1
RaulAngular <rblazquezi02@educarex.es>
2024-03-08 08:44:43 +0100

    9

---
+++ src_Conversor-monedas/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Angular Conversor de monedas';
+  euro: number = 0;
+  yen: number = 0;
+  dolar: number = 0;
+  e_y: number = 138.39;
+  e_d: number = 1.08;
+  d_e: number = 0.93;
+  d_y: number = 128.16;
+  y_e: number = 0.007;
+  y_d: number = 0.008;
+  cargarEuros() {
+    this.dolar = Number((this.euro * this.e_d).toFixed(3));
+    this.yen = Number((this.euro * this.e_y).toFixed(3));
+  }
+
+  cargarDolares() {
+    this.euro = Number((this.dolar * this.d_e).toFixed(3));
+    this.yen = Number((this.dolar * this.d_y).toFixed(3));
+  }
+
+  cargarYenes() {
+    this.euro = Number((this.yen * this.y_e).toFixed(3));
+    this.dolar = Number((this.yen * this.y_d).toFixed(3));
+  }
+}
